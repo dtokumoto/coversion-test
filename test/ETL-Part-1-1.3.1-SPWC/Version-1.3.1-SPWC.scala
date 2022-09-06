3e943eca8b8d8c44ce6dabb70381c18886c40dd2
d.tokumoto@gmail.com <d.tokumoto@gmail.com>
2022-09-06 09:23:50 +0000

    2nd commit

--- test/ETL-Part-1-1.3.1-SPWC/Version-1.3.1-SPWC.scala
+++ test/ETL-Part-1-1.3.1-SPWC/Version-1.3.1-SPWC.scala
@@ -3,6 +3,7 @@
 // MAGIC %md # Course: ETL-Part-1
 // MAGIC * Version 1.3.1-SPWC
 // MAGIC * Built 2020-02-20 04:16:10 UTC
+// MAGIC * Edit 20222-0906
 // MAGIC * Git revision: af8dfa716f88906b6cb91bd4ea36f775290a6a86
 // MAGIC 
 // MAGIC Copyright © 2020 Databricks, Inc.
