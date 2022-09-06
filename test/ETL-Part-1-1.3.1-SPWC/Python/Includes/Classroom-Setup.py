2659a3ab42297b21d9e7c7c4abc2c7acb828c129
d.tokumoto@gmail.com <d.tokumoto@gmail.com>
2022-09-06 09:14:57 +0000

    1st test commit

---
+++ test/ETL-Part-1-1.3.1-SPWC/Python/Includes/Classroom-Setup.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+
+spark.conf.set("com.databricks.training.module-name", "etl-part-1")
+displayHTML("Preparing learning environment...")
+
+# COMMAND ----------
+
+# MAGIC %run "./Common-Notebooks/Common"
+
+# COMMAND ----------
+
+displayHTML("All done!")
+
---
+++ test/ETL-Part-1-1.3.1-SPWC/Version-1.3.1-SPWC.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source
+// MAGIC 
+// MAGIC %md # Course: ETL-Part-1
+// MAGIC * Version 1.3.1-SPWC
+// MAGIC * Built 2020-02-20 04:16:10 UTC
+// MAGIC * Git revision: af8dfa716f88906b6cb91bd4ea36f775290a6a86
+// MAGIC 
+// MAGIC Copyright © 2020 Databricks, Inc.
